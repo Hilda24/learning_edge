7e73105c7ca785f8ef0d278314396d71abb314ae
Hilda24 <54671845+Hilda24@users.noreply.github.com>
2020-10-29 17:33:59 +0530

    Update app.py

--- Clean_csv/app.py
+++ Clean_csv/app.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import numpy as np
 
+df=pd.read_csv("test.csv")
+
+missing_values=["na","n/a","-"]
+df=pd.read_csv("test.py",na_values=missing_values)
+
+df['Science'].filna(0,inplace=True)
+df['Math'].filna(0,inplace=True)
+df['lang'].filna(0,inplace=True)
